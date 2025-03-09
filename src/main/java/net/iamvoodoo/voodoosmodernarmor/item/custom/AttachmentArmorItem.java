7a14eca24acb8dd9c6f5405a8fa671e77e7f48ff
CodingKitten-YT <roozendaalberend@gmail.com>
2025-03-09 15:28:11 +0100

    Still not working

--- src/main/java/net/iamvoodoo/voodoosmodernarmor/item/custom/AttachmentArmorItem.java
+++ src/main/java/net/iamvoodoo/voodoosmodernarmor/item/custom/AttachmentArmorItem.java
@@ -1,20 +1,38 @@
 package net.iamvoodoo.voodoosmodernarmor.item.custom;
 
+import com.vicmatskiv.pointblank.Nameable;
 import com.vicmatskiv.pointblank.attachment.Attachment;
+import com.vicmatskiv.pointblank.attachment.AttachmentCategory;
 import com.vicmatskiv.pointblank.attachment.AttachmentHost;
 import com.vicmatskiv.pointblank.feature.Feature;
+import com.vicmatskiv.pointblank.item.ItemExtra;
+import net.fabricmc.fabric.api.item.v1.EnchantingContext;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.ArmorMaterial;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.resource.featuretoggle.FeatureSet;
+import net.minecraft.text.Text;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.World;
 
 import java.util.Collection;
 import java.util.List;
 
-public class AttachmentArmorItem extends ArmorItem implements AttachmentHost {
+public class AttachmentArmorItem extends ArmorItem implements AttachmentHost, Nameable, ItemExtra {
     private String name;
 
     public AttachmentArmorItem(RegistryEntry<ArmorMaterial> material, Type type, Settings settings) {
         super(material, type, settings);
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
     }
 
     @Override
@@ -23,10 +41,6 @@ public class AttachmentArmorItem extends ArmorItem implements AttachmentHost {
     }
 
     @Override
-    public String getName() {
-        return this.name;
-    }
-
     public Collection<Feature> getFeatures() {
         return List.of();
     }
