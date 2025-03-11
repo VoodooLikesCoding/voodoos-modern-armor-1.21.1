048492b66342c288a6c9f6862caeed9d8c3ffee2
CodingKitten-YT <roozendaalberend@gmail.com>
2025-03-11 06:17:56 +0100

    Still not working...

--- src/main/java/net/iamvoodoo/voodoosmodernarmor/item/custom/AttachmentArmorItem.java
+++ src/main/java/net/iamvoodoo/voodoosmodernarmor/item/custom/AttachmentArmorItem.java
@@ -2,23 +2,12 @@ package net.iamvoodoo.voodoosmodernarmor.item.custom;
 
 import com.vicmatskiv.pointblank.Nameable;
 import com.vicmatskiv.pointblank.attachment.Attachment;
-import com.vicmatskiv.pointblank.attachment.AttachmentCategory;
 import com.vicmatskiv.pointblank.attachment.AttachmentHost;
 import com.vicmatskiv.pointblank.feature.Feature;
 import com.vicmatskiv.pointblank.item.ItemExtra;
-import net.fabricmc.fabric.api.item.v1.EnchantingContext;
-import net.minecraft.enchantment.Enchantment;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.ArmorMaterial;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
 import net.minecraft.registry.entry.RegistryEntry;
-import net.minecraft.resource.featuretoggle.FeatureSet;
-import net.minecraft.text.Text;
-import net.minecraft.util.Hand;
-import net.minecraft.util.TypedActionResult;
-import net.minecraft.world.World;
 
 import java.util.Collection;
 import java.util.List;
